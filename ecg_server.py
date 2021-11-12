ec42b31d538fad87c0e55517655215b88a19f7a5
Anuj Som <anuj.som@duke.edu>
2021-11-12 14:52:26 -0500

    added basic logging functionality under logging/ folder. Added logging/ folder to .gitignore

---
+++ ecg_server.py
@@ -0,0 +1,30 @@
+from typing import Type
+from flask import Flask, request, jsonify
+import requests
+from datetime import datetime as dt
+import logging
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def status():
+    """States that server is on feedback message
+
+    Returns:
+        string: "Server is on"
+    """
+    return "Server is on"
+
+
+def start_server():
+    app.run()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='./logging/ECG_server_log.log',
+                        filemode='w', level=logging.INFO)
+    start_server()
+    
+
