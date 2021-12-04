a03ffb8a20d6cbc8ef386e6ca44452538669fb86
Anuj Som <anuj.som@duke.edu>
2021-12-03 21:58:15 -0500

    created update_patient_fields method for request handling

--- api/post_new_patient_info_route.py
+++ api/post_new_patient_info_route.py
@@ -2,16 +2,11 @@ from __main__ import app
 from database_init import Patient
 from flask import Flask, request, jsonify
 from pymodm import errors as pymodm_errors
-from api.shared_methods import get_mrns_from_database, validate_dict_input
+from api.shared_methods import validate_dict_input
 from api.shared_methods import str_to_int
+from api.shared_methods import get_patient_from_db
 
 
-def get_patient_from_db(MRN):
-    try:
-        db_item = Patient.objects.raw({"_id": MRN}).first()
-    except pymodm_errors.DoesNotExist:
-        return False
-    return db_item
 
 
 @app.route('/api/post_new_patient_info', methods=['POST'])
@@ -22,7 +17,16 @@ def post_new_patient():
     if error_string is not True:
         return error_string, status_code
 
-    MRN = str_to_int(in_data["MRN"])[0]
+    MRN, error = str_to_int(in_data["MRN"])
+    if(error == False):
+        return "MRN cannot be cast to int", 400
+
+    update_patient_fields(MRN, in_data)
+
+    return "Patient added to database", 200
+
+
+def update_patient_fields(MRN, in_data):
     patient = get_patient_from_db(MRN)
     if(patient is False):       # No patient exists in db yet; create new one
         patient = Patient(MRN=MRN).save()
@@ -32,7 +36,9 @@ def post_new_patient():
     if 'ECG_Trace' in in_data.keys:
         patient.ECG_trace = in_data['ECG_Trace']
 
-    return "Patient added to database", 200
+
+
+
 
 # The upload may also include a name, medical image, and/or heart
 # rate & ECG image
