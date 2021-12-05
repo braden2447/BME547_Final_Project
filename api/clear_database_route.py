18d6c189c5dbfe949909ce5bc0c361af7701314d
Anuj Som <anuj.som@duke.edu>
2021-12-05 14:38:33 -0500

    added get request route to clear database, with password required

---
+++ api/clear_database_route.py
@@ -0,0 +1,10 @@
+from __main__ import app
+from database_init import clean_database
+
+
+@app.route('/api/clear_db/<Password>', methods=['GET'])
+def get_mrn_route(Password):
+    if Password != "BME547":
+        return "Password incorrect"
+    clean_database()
+    return "Cleaned Database", 200
